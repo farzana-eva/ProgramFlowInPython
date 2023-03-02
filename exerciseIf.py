a96e7f5ee504af88a7a1efbfc696912696701058
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-03-01 23:55:26 -0500

    more python files added

---
+++ exerciseIf.py
@@ -0,0 +1,9 @@
+tree1 = "Mapple"
+tree2 = "christmas"
+
+# add the code to compare the trees
+if tree1 == tree2:
+    print("The trees are the same.")
+
+else:
+    print("The trees are different.")
